aaf0befddac8f7643539574edfb9e1fc9ff5a7ac
EduViera131 <eduviera369@gmail.com>
2023-07-07 15:34:19 -0300

    :fix: task list error

--- src/components/Editor/Editor.tsx
+++ src/components/Editor/Editor.tsx
@@ -7,8 +7,6 @@ import {
 import StarterKit from '@tiptap/starter-kit'
 import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
 import js from 'highlight.js/lib/languages/javascript'
-import TaskItem from '@tiptap/extension-task-item'
-import TaskList from '@tiptap/extension-task-list'
 import { initialContent } from './initialContent'
 import { lowlight } from 'lowlight'
 import {
